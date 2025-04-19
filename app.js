d4a7f9de40e7529a6ac5388cd2a9b5da107a1259
sksayril <sksayril123@gmail.com>
2025-04-19 15:20:09 +0530

    entity long

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json({limit: '500mb'}));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 
 app.use(express.json());
