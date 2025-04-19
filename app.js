1f144b4a2f46906b32f3a708227379f75465d397
sksayril <sksayril123@gmail.com>
2025-04-19 15:18:43 +0530

    chnages

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json({limit: '500mb'}));
 
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
